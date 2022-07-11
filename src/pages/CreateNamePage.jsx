5c331e2d03b4fe427d4df24664867d810a4ed7bf
Juan Martinez <mrjoanmtx@gmail.com>
2022-07-11 17:17:18 -0500

    [update] user can delete all string in create name

--- src/pages/CreateNamePage.jsx
+++ src/pages/CreateNamePage.jsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux';
 
 import { NameBreakdown, UnselectedConsultant,
@@ -15,11 +15,11 @@ import moment from 'moment';
 const CreateNamePage = () => {
   const { userActive } = useSelector(state => state.users);
   const isEmpty = Object.keys(userActive).length === 0;
+  const [isEdditing, setIsEdditing] = useState(false)
 
   const { names:name, lastName, scdLastName, date: birthDate } = userActive
   const { name: createName, date: createDate } = useSelector(state => state.users.createName)
 
-  // console.log( {createName, createDate} )
   const dispatch = useDispatch()
 
   const inputName = useRef()
@@ -28,11 +28,12 @@ const CreateNamePage = () => {
   if( isEmpty ){
     return <UnselectedConsultant />
   }
+
   const createNameData = {
-    name: createName == '' ? `${name} ${lastName} ${scdLastName}` : createName,
+    name: ! isEdditing ? `${name} ${lastName} ${scdLastName}` : createName,
     lastName: '',
     scdLastName: '',
-    birthDate: createDate == '' ? birthDate : createDate,
+    birthDate: ! isEdditing ? birthDate : createDate,
   }
   const createNameObj = new Person( createNameData )
 
@@ -49,11 +50,18 @@ const CreateNamePage = () => {
   const nextYear = moment().add(1, 'year')
   const annualReturnNextYear = createNameObj.annualReturn(nextYear.year())
 
+  const isValid = () => {
+    if( createNameData.name === '' ) return false
+    if( createNameData.birthDate === '' ) return false
+    if( createNameData.name === '' && createNameData.birthDate === '' ) return false
+    return true
+  }
+
   const handleChange = () => {
+    setIsEdditing(true)
     const name = inputName.current.value
     const birthDate = inputDate.current.value
     dispatch(setCreateName({name, date: birthDate}))
-    console.log( { name, birthDate } )
   }
 
   return(
@@ -101,124 +109,132 @@ const CreateNamePage = () => {
           </div>
         </div>
 
-        <div className='col-span-8 mb-5'>
-          <div className='bg-black text-white text-base font-bold h-8 flex justify-start items-center rounded-tl-2xl rounded-tr-2xl'>
-            <div className='w-9 h-9 flex justify-center items-center rounded-full -ml-3 mr-2 bg-blue p-2'>
-              <TiPlus className='text-2xl'/>
-            </div>
-            Valores Numéricos del Nombre
-          </div>
-          <div className='pinnacle-wrap px-8 py-8'>
-            <div className='flex justify-between'>
-              <div className='flex flex-col items-center justify-center text-gray-500 font-bold'>
-                <label className='text-13 mb-3'>Nombre</label>
-                <div className='w-18 h-18 text-3xl font-black text-black flex justify-center items-center bg-blue-30 border border-blue rounded-full inner-shadow'>
-                  {createNameObj.calcName()}{createNameObj.calcNameISK()}
+        {
+          isValid() ?
+            <>
+              <div className='col-span-8 mb-5'>
+                <div className='bg-black text-white text-base font-bold h-8 flex justify-start items-center rounded-tl-2xl rounded-tr-2xl'>
+                  <div className='w-9 h-9 flex justify-center items-center rounded-full -ml-3 mr-2 bg-blue p-2'>
+                    <TiPlus className='text-2xl'/>
+                  </div>
+                  Valores Numéricos del Nombre
                 </div>
-              </div>
-              <div className='flex flex-col items-center justify-center text-gray-500 font-bold'>
-                <label className='text-13 mb-3'>Alma</label>
-                <div className='w-18 h-18 text-3xl font-black text-black flex justify-center items-center bg-blue-30 border border-blue rounded-full inner-shadow-gold'>
-                  {createNameObj.calcSoulNumber()}{createNameObj.calcSoulNumberISK()}
-                </div>
-              </div>
-              <div className='flex flex-col items-center justify-center text-gray-500 font-bold'>
-                <label className='text-13 mb-3'>Expresión</label>
-                <div className='w-18 h-18 text-3xl font-black text-black flex justify-center items-center bg-blue-30 border border-blue rounded-full inner-shadow'>
-                  {createNameObj.calcSoulExpresion()}{createNameObj.calcSoulExpresionISK()}
-                </div>
-              </div>
-              <div className='flex flex-col items-center justify-center text-gray-500 font-bold'>
-                <label className='text-13 mb-3'>Madurez</label>
-                <div className='w-18 h-18 text-3xl font-black text-black flex justify-center items-center bg-aguamarina-30 border border-aguamarina rounded-full inner-shadow'>
-                  {createNameObj.calcSoulExpresion()}{createNameObj.calcSoulExpresionISK()}
-                </div>
-              </div>
-              <div className='flex flex-col items-center justify-center text-gray-500 font-bold'>
-                <label className='text-13 mb-3'>Dígito Edad</label>
-                <div className='w-18 h-18 text-3xl font-black text-black flex justify-center items-center bg-white border border-blue rounded-full inner-shadow'>
-                  {createNameObj.calcOneDigitYearsOld()}
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className='col-span-4 row-span-2 mb-5'>
-          <div className='bg-black text-white text-base font-bold h-8 flex justify-start items-center rounded-tl-2xl rounded-tr-2xl'>
-            <div className='w-9 h-9 flex justify-center items-center rounded-full -ml-3 mr-2 bg-main p-2'>
-              <TiPlus className='text-2xl'/>
-            </div>
-            Pináculo
-          </div>
-          <div className='pinnacle-wrap px-8 py-3'>
-            <Pinnacle consultant={createNameObj} size="small" />
-          </div>
-        </div>
-
-        <div className='col-span-8 mb-5'>
-          <div className='bg-black text-white text-base font-bold h-8 flex justify-start items-center rounded-tl-2xl rounded-tr-2xl'>
-            <div className='w-9 h-9 flex justify-center items-center rounded-full -ml-3 mr-2 bg-blue p-2'>
-              <TiPlus className='text-2xl'/>
-            </div>
-            Tabla de inclusión
-          </div>
-          <div className='pinnacle-wrap px-8 py-10'>
-            <div className='grid grid-cols-10 gap-3'>
-              <div className=' gap-4 flex justify-center items-center flex-col'>
-                <div className='h-5'></div>
-                <div className='w-full col-start-1 row-start-2 col-span-2 h-10 text-13 font-black text-gray-400 flex justify-center items-center'>
-                  Casas:
-                </div>
-                <div className='w-full col-start-1 row-start-3 col-span-2 h-10 text-13 font-black text-gray-400 flex justify-center items-center'>
-                  Habs:
+                <div className='pinnacle-wrap px-8 py-8'>
+                  <div className='flex justify-between'>
+                    <div className='flex flex-col items-center justify-center text-gray-500 font-bold'>
+                      <label className='text-13 mb-3'>Nombre</label>
+                      <div className='w-18 h-18 text-3xl font-black text-black flex justify-center items-center bg-blue-30 border border-blue rounded-full inner-shadow'>
+                        {createNameObj.calcName()}{createNameObj.calcNameISK()}
+                      </div>
+                    </div>
+                    <div className='flex flex-col items-center justify-center text-gray-500 font-bold'>
+                      <label className='text-13 mb-3'>Alma</label>
+                      <div className='w-18 h-18 text-3xl font-black text-black flex justify-center items-center bg-blue-30 border border-blue rounded-full inner-shadow-gold'>
+                        {createNameObj.calcSoulNumber()}{createNameObj.calcSoulNumberISK()}
+                      </div>
+                    </div>
+                    <div className='flex flex-col items-center justify-center text-gray-500 font-bold'>
+                      <label className='text-13 mb-3'>Expresión</label>
+                      <div className='w-18 h-18 text-3xl font-black text-black flex justify-center items-center bg-blue-30 border border-blue rounded-full inner-shadow'>
+                        {createNameObj.calcSoulExpresion()}{createNameObj.calcSoulExpresionISK()}
+                      </div>
+                    </div>
+                    <div className='flex flex-col items-center justify-center text-gray-500 font-bold'>
+                      <label className='text-13 mb-3'>Madurez</label>
+                      <div className='w-18 h-18 text-3xl font-black text-black flex justify-center items-center bg-aguamarina-30 border border-aguamarina rounded-full inner-shadow'>
+                        {createNameObj.calcSoulExpresion()}{createNameObj.calcSoulExpresionISK()}
+                      </div>
+                    </div>
+                    <div className='flex flex-col items-center justify-center text-gray-500 font-bold'>
+                      <label className='text-13 mb-3'>Dígito Edad</label>
+                      <div className='w-18 h-18 text-3xl font-black text-black flex justify-center items-center bg-white border border-blue rounded-full inner-shadow'>
+                        {createNameObj.calcOneDigitYearsOld()}
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
 
-              { Object.entries(createNameObj.getAppearances()).map( (el, i) =>
-                <div key={i} className='gap-4 flex justify-center items-center flex-col'>
-                  <div className='text-13 text-gray-500 h-5'>{el[1].v} </div>
-                  <div className='h-10 w-10 text-xl font-bold flex justify-center items-center bg-purple-30 border border-main rounded-md inner-shadow'>{el[0]} </div>
-                  <div className='h-10 w-10 text-xl font-bold flex justify-center items-center bg-gray-300 border border-gray-500 rounded-md inner-shadow'>{el[1].a} </div>
+              <div className='col-span-4 row-span-2 mb-5'>
+                <div className='bg-black text-white text-base font-bold h-8 flex justify-start items-center rounded-tl-2xl rounded-tr-2xl'>
+                  <div className='w-9 h-9 flex justify-center items-center rounded-full -ml-3 mr-2 bg-main p-2'>
+                    <TiPlus className='text-2xl'/>
+                  </div>
+                  Pináculo
                 </div>
-              )}
-            </div>
-          </div>
-        </div>
+                <div className='pinnacle-wrap px-8 py-3'>
+                  <Pinnacle consultant={createNameObj} size="small" />
+                </div>
+              </div>
 
-        <div className='col-span-12 mb-5'>
-          <div className='bg-black text-white text-base font-bold h-8 flex justify-start items-center rounded-tl-2xl rounded-tr-2xl'>
-            <div className='w-9 h-9 flex justify-center items-center rounded-full -ml-3 mr-2 bg-blue p-2'>
-              <TiPlus className='text-2xl'/>
-            </div>
-            Desglose del Nombre
-            <MdEdit className='ml-2 text-2xl'/>
-          </div>
-          <div className='pinnacle-wrap px-8 py-8'>
-            <NameBreakdown name={ungroupName} values={ungroupNameV} total={ungroupNameT} description="NP" />
-          </div>
-        </div>
+              <div className='col-span-8 mb-5'>
+                <div className='bg-black text-white text-base font-bold h-8 flex justify-start items-center rounded-tl-2xl rounded-tr-2xl'>
+                  <div className='w-9 h-9 flex justify-center items-center rounded-full -ml-3 mr-2 bg-blue p-2'>
+                    <TiPlus className='text-2xl'/>
+                  </div>
+                  Tabla de inclusión
+                </div>
+                <div className='pinnacle-wrap px-8 py-10'>
+                  <div className='grid grid-cols-10 gap-3'>
+                    <div className=' gap-4 flex justify-center items-center flex-col'>
+                      <div className='h-5'></div>
+                      <div className='w-full col-start-1 row-start-2 col-span-2 h-10 text-13 font-black text-gray-400 flex justify-center items-center'>
+                        Casas:
+                      </div>
+                      <div className='w-full col-start-1 row-start-3 col-span-2 h-10 text-13 font-black text-gray-400 flex justify-center items-center'>
+                        Habs:
+                      </div>
+                    </div>
 
-        <div className='col-span-12 mb-5'>
-          <div className='bg-black text-white text-base font-bold h-8 flex justify-start items-center rounded-tl-2xl rounded-tr-2xl'>
-            <div className='w-9 h-9 flex justify-center items-center rounded-full -ml-3 mr-2 bg-green-s p-2'>
-              <TiPlus className='text-2xl'/>
-            </div>
-            Retornos Anuales
-            <MdEdit className='ml-2 text-2xl'/>
-          </div>
-          <div className='pinnacle-wrap overflow-hidden grid grid-cols-3'>
-            <div className='px-5 py-8'>
-              <AnnualReturn annualReturn={annualReturnPastYear} />
-            </div>
-            <div className='px-5 py-8 border-b border-solid border-gray-300 bg-active-radial bg-opacity-15'>
-              <AnnualReturn annualReturn={annualReturnCurrent} current months />
-            </div>
-            <div className='px-5 py-8 border-r border-gray-400'>
-              <AnnualReturn annualReturn={annualReturnNextYear} />
-            </div>
-          </div>
-        </div>
+                    { Object.entries(createNameObj.getAppearances()).map( (el, i) =>
+                      <div key={i} className='gap-4 flex justify-center items-center flex-col'>
+                        <div className='text-13 text-gray-500 h-5'>{el[1].v} </div>
+                        <div className='h-10 w-10 text-xl font-bold flex justify-center items-center bg-purple-30 border border-main rounded-md inner-shadow'>{el[0]} </div>
+                        <div className='h-10 w-10 text-xl font-bold flex justify-center items-center bg-gray-300 border border-gray-500 rounded-md inner-shadow'>{el[1].a} </div>
+                      </div>
+                    )}
+                  </div>
+                </div>
+              </div>
+
+              <div className='col-span-12 mb-5'>
+                <div className='bg-black text-white text-base font-bold h-8 flex justify-start items-center rounded-tl-2xl rounded-tr-2xl'>
+                  <div className='w-9 h-9 flex justify-center items-center rounded-full -ml-3 mr-2 bg-blue p-2'>
+                    <TiPlus className='text-2xl'/>
+                  </div>
+                  Desglose del Nombre
+                  <MdEdit className='ml-2 text-2xl'/>
+                </div>
+                <div className='pinnacle-wrap px-8 py-8'>
+                  <NameBreakdown name={ungroupName} values={ungroupNameV} total={ungroupNameT} description="NP" />
+                </div>
+              </div>
+
+              <div className='col-span-12 mb-5'>
+                <div className='bg-black text-white text-base font-bold h-8 flex justify-start items-center rounded-tl-2xl rounded-tr-2xl'>
+                  <div className='w-9 h-9 flex justify-center items-center rounded-full -ml-3 mr-2 bg-green-s p-2'>
+                    <TiPlus className='text-2xl'/>
+                  </div>
+                  Retornos Anuales
+                  <MdEdit className='ml-2 text-2xl'/>
+                </div>
+                <div className='pinnacle-wrap overflow-hidden grid grid-cols-3'>
+                  <div className='px-5 py-8'>
+                    <AnnualReturn annualReturn={annualReturnPastYear} />
+                  </div>
+                  <div className='px-5 py-8 border-b border-solid border-gray-300 bg-active-radial bg-opacity-15'>
+                    <AnnualReturn annualReturn={annualReturnCurrent} current months />
+                  </div>
+                  <div className='px-5 py-8 border-r border-gray-400'>
+                    <AnnualReturn annualReturn={annualReturnNextYear} />
+                  </div>
+                </div>
+              </div>
+            </>
+          :
+          <div className="col-span-12 text-center font-bold">Ingresa datos validos.</div>
+        }
+
       </div>
 
     </>
