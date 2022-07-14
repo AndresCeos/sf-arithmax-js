f5ff79168dc2cec39b29b5da4aef8450bcf01b11
JuanMtx20 <mrjoanmtx@gmail.com>
2022-07-14 12:43:28 -0500

    [fix] partner info

--- src/pages/SinastryPage.jsx
+++ src/pages/SinastryPage.jsx
@@ -103,45 +103,6 @@ const SinastryPage = () => {
               <div className='flex flex-col items-center justify-center text-gray-500 font-bold'>
                 <label className='mb-3 text-gray text-13'>Nombre</label>
                 <CircleNumber size="sm" appearance="blue-30" border="blue">
-                  {partner.calcName()}{partner.calcNameISK()}
-                </CircleNumber>
-              </div>
-              <div className='flex flex-col items-center justify-center text-gray-500 font-bold'>
-                <label className='mb-3 text-gray text-13'>Alma</label>
-                <CircleNumber size="sm" appearance="blue-30" border="blue" radiant>
-                  {partner.calcSoulNumber()}{partner.calcSoulNumberISK()}
-                </CircleNumber>
-              </div>
-              <div className='flex flex-col items-center justify-center text-gray-500 font-bold'>
-                <label className='mb-3 text-gray text-13'>Expresión</label>
-                <CircleNumber size="sm" appearance="blue-30" border="blue">
-                  {partner.calcSoulExpresion()}{partner.calcSoulExpresionISK()}
-                </CircleNumber>
-              </div>
-              <div className='flex flex-col items-center justify-center text-gray-500 font-bold'>
-                <label className='mb-3 text-gray text-13'>Madurez</label>
-                <CircleNumber size="sm" appearance="aguamarina-30" border="aguamarina">
-                  {partner.calcMaturity()}{partner.calcMaturityISK()}
-                </CircleNumber>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className='col-span-4 mb-1'>
-          <div className='bg-black text-white text-base font-bold h-8 flex items-center justify-between rounded-tl-2xl rounded-tr-2xl'>
-            <div className='flex items-center'>
-              <div className='w-9 h-9 flex justify-center items-center rounded-full -ml-3 mr-2 bg-blue p-2'>
-                <TiPlus className='text-2xl'/>
-              </div>
-              Nombre: {consultant.name}
-            </div>
-          </div>
-          <div className='pinnacle-wrap px-5 py-4'>
-            <div className='grid grid-cols-4'>
-              <div className='flex flex-col items-center justify-center text-gray-500 font-bold'>
-                <label className='mb-3 text-gray text-13'>Nombre</label>
-                <CircleNumber size="sm" appearance="blue-30" border="blue">
                   {consultant.calcName()}{consultant.calcNameISK()}
                 </CircleNumber>
               </div>
@@ -155,6 +116,45 @@ const SinastryPage = () => {
                 <label className='mb-3 text-gray text-13'>Expresión</label>
                 <CircleNumber size="sm" appearance="blue-30" border="blue">
                   {consultant.calcSoulExpresion()}{consultant.calcSoulExpresionISK()}
+                </CircleNumber>
+              </div>
+              <div className='flex flex-col items-center justify-center text-gray-500 font-bold'>
+                <label className='mb-3 text-gray text-13'>Madurez</label>
+                <CircleNumber size="sm" appearance="aguamarina-30" border="aguamarina">
+                  {consultant.calcMaturity()}{consultant.calcMaturityISK()}
+                </CircleNumber>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className='col-span-4 mb-1'>
+          <div className='bg-black text-white text-base font-bold h-8 flex items-center justify-between rounded-tl-2xl rounded-tr-2xl'>
+            <div className='flex items-center'>
+              <div className='w-9 h-9 flex justify-center items-center rounded-full -ml-3 mr-2 bg-blue p-2'>
+                <TiPlus className='text-2xl'/>
+              </div>
+              Nombre: {partner.name}
+            </div>
+          </div>
+          <div className='pinnacle-wrap px-5 py-4'>
+            <div className='grid grid-cols-4'>
+              <div className='flex flex-col items-center justify-center text-gray-500 font-bold'>
+                <label className='mb-3 text-gray text-13'>Nombre</label>
+                <CircleNumber size="sm" appearance="blue-30" border="blue">
+                  {partner.calcName()}{partner.calcNameISK()}
+                </CircleNumber>
+              </div>
+              <div className='flex flex-col items-center justify-center text-gray-500 font-bold'>
+                <label className='mb-3 text-gray text-13'>Alma</label>
+                <CircleNumber size="sm" appearance="blue-30" border="blue" radiant>
+                  {partner.calcSoulNumber()}{partner.calcSoulNumberISK()}
+                </CircleNumber>
+              </div>
+              <div className='flex flex-col items-center justify-center text-gray-500 font-bold'>
+                <label className='mb-3 text-gray text-13'>Expresión</label>
+                <CircleNumber size="sm" appearance="blue-30" border="blue">
+                  {partner.calcSoulExpresion()}{partner.calcSoulExpresionISK()}
                 </CircleNumber>
               </div>
               <div className='flex flex-col items-center justify-center text-gray-500 font-bold'>
