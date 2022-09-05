2f434797e4b6bc107d1d1c11d46e9028bd63776d
JuanMtx20 <mrjoanmtx@gmail.com>
2022-09-05 11:17:59 -0500

    [fix] global

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import shimReactPdf from "vite-plugin-shim-react-pdf";
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import shimReactPdf from 'vite-plugin-shim-react-pdf';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), shimReactPdf()]
+  plugins: [react(), shimReactPdf()],
+  global: {},
 })
