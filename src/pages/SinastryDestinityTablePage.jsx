24b503de576800a80333e27d2fd8cb9d077d5aa7
Juan Martinez <66894611+JuanMtx20@users.noreply.github.com>
2022-09-14 18:15:39 -0500

    fix/destinity-freezed (#7)

--- src/pages/SinastryDestinityTablePage.jsx
+++ src/pages/SinastryDestinityTablePage.jsx
@@ -1,27 +1,37 @@
 import { useDispatch, useSelector } from 'react-redux';
 
-import { UserPartnerSelect, SinastryDestinyTable, UnselectedConsultant } from '../components'
+import { SinastryDestinyTable, UnselectedConsultant, UserPartnerSelect } from '../components';
 
-import moment from 'moment';
-import { Person } from '../resources'
+import { Person } from '../resources';
 
-import { TiPlus } from "react-icons/ti";
+import { TiPlus } from 'react-icons/ti';
 import { useConsultant } from '../hooks';
 import { setIsSelectPartner } from '../store/slices/users/users';
 
 const SinastyDestinityTablePage = () => {
   const { userActive, userPartnerActive, isSelectPartner } = useSelector(state => state.users);
-  const isEmpty = Object.keys(userActive).length === 0;
-
   const { consultant } = useConsultant()
   const dispatch = useDispatch()
 
-  if( isEmpty ){
-    return<UnselectedConsultant />
+  const isUnselectedConsultant = Object.keys(userActive).length === 0;
+  if (isUnselectedConsultant) {
+    return <UnselectedConsultant />
   }
-  const isEmptyP = Object.keys(userActive.partner).length === 0;
-  if(isEmptyP){
-    dispatch( setIsSelectPartner(false))
+
+  const hasPartners = Object.keys(userActive.partner).length > 0;
+  if (!hasPartners) {
+    dispatch(setIsSelectPartner(false))
+  }
+
+  const isSelectedPartner = Object.keys(userPartnerActive).length > 0;
+
+  if (!isSelectedPartner) {
+    return (
+      <div className='grid grid-cols-12 mx-14 gap-6 mt-8 pt-10'>
+        <UserPartnerSelect />
+        <div className="col-span-12 text-center text-black">Agrega/Selecciona una pareja para ver esta información</div>
+      </div>
+    )
   }
 
   const partner = new Person({
@@ -29,45 +39,31 @@ const SinastyDestinityTablePage = () => {
     lastName: userPartnerActive.lastName,
     scdLastName: userPartnerActive.scdLastName,
     birthDate: userPartnerActive.date,
-    yearMeet :userPartnerActive.yearMeet
+    yearMeet: userPartnerActive.yearMeet
   })
-  console.log(isSelectPartner)
-  console.log(userPartnerActive)
 
-  if( partner === undefined ){
-    return<UnselectedConsultant />
-  }
-  const isEmptyGroup = Object.keys(userActive.partner).length === 0;
-  if( isEmptyGroup ){
-    return (
-      <div className='grid grid-cols-12 mx-14 gap-6 mt-8 pt-10'><UserPartnerSelect />
-      <div className="col-span-12 text-center"><strong>Agrega/Selecciona una pareja para ver esta información</strong></div></div>)
+  if (partner === undefined) {
+    return <UnselectedConsultant />
   }
 
-  // console.log( moment(userPartnerActive.yearMeet).year() )
-  // console.log( consultant.birthDate.year() )
-  // const ageMeet = moment(userPartnerActive.yearMeet).year() - consultant.birthDate.year()
   const ageMeet = userPartnerActive.yearMeet - consultant.birthDate.year()
   const t = consultant.getDestinityTable()
   const table = t.slice(ageMeet)
-  // console.log(table)
 
   const table1 = table.slice(0, 11);
   const table2 = table.slice(11, 22);
   const table3 = table.slice(22, 33);
   const table4 = table.slice(33, 44);
 
-   // console.log( moment(userPartnerActive.yearMeet).year() )
-  // console.log( partner.birthDate.year() )
-  // const ageMeetP = moment(userPartnerActive.yearMeet).year() - partner.birthDate.year()
   const ageMeetP = userPartnerActive.yearMeet - partner.birthDate.year()
   let tP
   let partnerTable = []
-  let  partnerTable1 = []
-  let  partnerTable2 = []
-  let  partnerTable3 = []
-  let  partnerTable4 = []
-  if(isSelectPartner){
+  let partnerTable1 = []
+  let partnerTable2 = []
+  let partnerTable3 = []
+  let partnerTable4 = []
+
+  if (isSelectedPartner) {
     tP = partner.getDestinityTable()
     partnerTable = tP.slice(ageMeetP)
     partnerTable1 = partnerTable.slice(0, 11);
@@ -75,21 +71,20 @@ const SinastyDestinityTablePage = () => {
     partnerTable3 = partnerTable.slice(22, 33);
     partnerTable4 = partnerTable.slice(33, 44);
   }
-  // console.log(partnerTable)
-  // const partnerTable = partner.getDestinityTable()
 
 
-  return(
+  return (
     <div className='grid grid-cols-12 mx-14 gap-6 mt-8 pt-10'>
 
-        <UserPartnerSelect/>
+      <UserPartnerSelect />
 
-        {(isSelectPartner)?
+      {(isSelectedPartner)
 
+        ? (
           <div className='col-span-12 mb-5'>
             <div className='bg-black text-white text-base font-bold h-8 flex justify-start items-center rounded-tl-2xl rounded-tr-2xl'>
               <div className='w-9 h-9 flex justify-center items-center rounded-full -ml-3 mr-2 bg-gold p-2'>
-                <TiPlus className='text-2xl'/>
+                <TiPlus className='text-2xl' />
               </div>
               Tabla del Destino de la Pareja
             </div>
@@ -102,7 +97,7 @@ const SinastyDestinityTablePage = () => {
                 partner={partner}
                 tableP={partnerTable1}
                 startP={0 + ageMeetP}
-                />
+              />
               <SinastryDestinyTable
                 table={table2}
                 start={11 + ageMeet}
@@ -110,7 +105,7 @@ const SinastyDestinityTablePage = () => {
                 partner={partner}
                 tableP={partnerTable2}
                 startP={11 + ageMeetP}
-                />
+              />
               <SinastryDestinyTable
                 table={table3}
                 start={22 + ageMeet}
@@ -118,7 +113,7 @@ const SinastyDestinityTablePage = () => {
                 partner={partner}
                 tableP={partnerTable3}
                 startP={22 + ageMeetP}
-                />
+              />
               <SinastryDestinyTable
                 table={table4}
                 start={33 + ageMeet}
@@ -126,12 +121,12 @@ const SinastyDestinityTablePage = () => {
                 partner={partner}
                 tableP={partnerTable4}
                 startP={33 + ageMeetP}
-                />
+              />
             </div>
           </div>
-        :
-        <div className="col-span-12 text-center"><strong>Agrega/Selecciona una pareja para ver esta información</strong></div>
-        }
+        )
+        : <div className="col-span-12 text-center">Agrega/Selecciona una pareja para ver esta información</div>
+      }
 
     </div>
   )
