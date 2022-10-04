e8e550d312209271027c82b3bf7f3c04eb20ec2c
Ceosnewmedia <juan@ceosnm.com>
2022-10-03 22:57:58 -0500

    feature/active-colors-destinity-table
    Co-authored-by: JuanMtx20 <mrjoanmtx@gmail.com>

--- src/components-pdf/destinityTable/DestinityTable.jsx
+++ src/components-pdf/destinityTable/DestinityTable.jsx
@@ -1,7 +1,30 @@
 import { StyleSheet, Text, View } from '@react-pdf/renderer';
 
-export const DestinityTable = ({ consultant, date, table, slice = 0, start = 0 }) => {
+export const DestinityTable = ({ consultant, newDate, nameCycles, nameSubCycles, table, slice = 0, start = 0 }) => {
   const single = consultant.getSingle()
+  console.log({ consultant, newDate, table, slice, start })
+
+  const consultantAge = consultant.getYearsOld(newDate.year())
+  const isCycle = i => {
+    return i === consultantAge ? false : nameCycles.includes(i)
+  }
+  const isSubCycle = i => {
+    return i === consultantAge ? false : nameSubCycles.includes(i)
+  }
+  const bkConfig = (i, bg) => {
+    console.log({ i, consultantAge })
+    if (i === consultantAge) {
+      return '#b95253cc'
+    }
+    if (isCycle(i)) {
+      return '#FBEDD9'
+    }
+    if (isSubCycle(i)) {
+      return '#FFFDEA'
+    }
+    return bg
+  }
+
   return (
     <View style={pinnacleName.container}>
       <View style={pinnacleName.wrap}>
@@ -49,57 +72,57 @@ export const DestinityTable = ({ consultant, date, table, slice = 0, start = 0 }
           {table.map((el, i) => (
             // eslint-disable-next-line react/no-array-index-key
             <View key={`${consultant.getYearOfBirth() + i + start}_${i}`} style={{ position: 'absolute', left: 81 + (i * 19), top: 0 }}>
-              <View style={[pinnacleName.item, { width: 19, height: 15, backgroundColor: '#c2b3c2' }]}>
+              <View style={[pinnacleName.item, { width: 19, height: 15, backgroundColor: bkConfig(i + start, '#c2b3c2') }]}>
                 <Text>
                   {consultant.getYearOfBirth() + i + start}
                 </Text>
               </View>
-              <View style={[pinnacleName.item, { width: 19, height: 15, backgroundColor: '#e5e5e5' }]}>
+              <View style={[pinnacleName.item, { width: 19, height: 15, backgroundColor: bkConfig(i + start, '#e5e5e5') }]}>
                 <Text>
                   {i + start}
                 </Text>
               </View>
-              <View style={[pinnacleName.item, { width: 19, height: 15, backgroundColor: '#ffffff' }]}>
+              <View style={[pinnacleName.item, { width: 19, height: 15, backgroundColor: bkConfig(i + start, '#ffffff') }]}>
                 <Text>
                   {el.pmC}
                 </Text>
               </View>
-              <View style={[pinnacleName.item, { width: 19, height: 16, fontSize: 6, backgroundColor: '#ffffff' }]}>
+              <View style={[pinnacleName.item, { width: 19, height: 16, fontSize: 6, backgroundColor: bkConfig(i + start, '#ffffff') }]}>
                 <Text>
                   {el.pmN}/{el.pmD}
                 </Text>
               </View>
-              <View style={[pinnacleName.item, { width: 19, height: 15, backgroundColor: '#ffffff' }]}>
+              <View style={[pinnacleName.item, { width: 19, height: 15, backgroundColor: bkConfig(i + start, '#ffffff') }]}>
                 <Text>
                   {el.pMC}
                 </Text>
               </View>
-              <View style={[pinnacleName.item, { width: 19, height: 16, fontSize: 6, backgroundColor: '#ffffff' }]}>
+              <View style={[pinnacleName.item, { width: 19, height: 16, fontSize: 6, backgroundColor: bkConfig(i + start, '#ffffff') }]}>
                 <Text>
                   {el.pMN}/{el.pMD}
                 </Text>
               </View>
-              <View style={[pinnacleName.item, { width: 19, height: 15, backgroundColor: '#ffffff' }]}>
+              <View style={[pinnacleName.item, { width: 19, height: 15, backgroundColor: bkConfig(i + start, '#ffffff') }]}>
                 <Text>
                   {single && el.pfC}
                 </Text>
               </View>
-              <View style={[pinnacleName.item, { width: 19, height: 16, fontSize: 6, backgroundColor: '#ffffff' }]}>
+              <View style={[pinnacleName.item, { width: 19, height: 16, fontSize: 6, backgroundColor: bkConfig(i + start, '#ffffff') }]}>
                 <Text>
                   {single && `${el.pfN}/${el.pfD}`}
                 </Text>
               </View>
-              <View style={[pinnacleName.item, { width: 19, height: 25, backgroundColor: '#edd7eb' }]}>
+              <View style={[pinnacleName.item, { width: 19, height: 25, backgroundColor: bkConfig(i + start, '#edd7eb') }]}>
                 <Text>
                   {consultant.reduceNumber(el.pmD + el.pMD + (single ? el.pfN : 0))}
                 </Text>
               </View>
-              <View style={[pinnacleName.item, { width: 19, height: 25, backgroundColor: '#ededed', marginTop: 10 }]}>
+              <View style={[pinnacleName.item, { width: 19, height: 25, backgroundColor: bkConfig(i + start, '#ededed'), marginTop: 10 }]}>
                 <Text>
                   {consultant.calcPersonalYear(consultant.getYearOfBirth() + i + start)}
                 </Text>
               </View>
-              <View style={[pinnacleName.item, { width: 19, height: 25, backgroundColor: '#ffffff' }]}>
+              <View style={[pinnacleName.item, { width: 19, height: 25, backgroundColor: bkConfig(i + start, '#ffffff') }]}>
                 <Text>
                   {consultant.reduceNumber(el.pmD + el.pMD + (single ? el.pfD : 0) + consultant.calcPersonalYear(consultant.getYearOfBirth() + i + start))}
                 </Text>
