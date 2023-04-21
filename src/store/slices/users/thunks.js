3e55d76d85860973e67f76063f5c3b9f5dc920f8
DOC-weed <aleesfeo1234@gmail.com>
2023-04-21 14:11:34 -0600

    [fix] mac update fix

--- src/store/slices/users/thunks.js
+++ src/store/slices/users/thunks.js
@@ -5,7 +5,9 @@ import { expired, logout } from '../auth/authSlice';
 import { setUserList } from './users';
 
 export const checkAvailabilityDevices = async (dispatch) => {
-  if (!localStorage.getItem('loading') || localStorage.getItem('loading') === 'false') {
+  console.log('this is working');
+  console.log(localStorage.getItem('app_version'))
+  if (localStorage.getItem('app_version') !== '2.0.8' || localStorage.getItem('loading') === 'false') {
     try {
       const { token } = await localForage.getItem('session');
       const m = localStorage.getItem('m');
@@ -16,6 +18,7 @@ export const checkAvailabilityDevices = async (dispatch) => {
       const restApiUrl = `${clientConfig.siteUrl}/wp-json/app/v1/d`;
       axios.post(restApiUrl, { m }, config)
         .then(res => {
+          console.log({ app_version: res.data.app_version })
           localStorage.setItem('loading', false);
           localForage.getItem('users', (err, users) => {
             console.log({ users })
